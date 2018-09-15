d93077732ba8278435333d998a67cbb3e9138885
daoyijushi <chanshuikongxin@163.com>
2018-09-15 19:54:44 +0800

    Add files via upload
    重命图片文件名称

---
+++ rename_label.py
@@ -0,0 +1,14 @@
+
+
+from glob import glob
+import os
+files = glob("./*/label.png")
+i = 0
+for file in files:
+    i = i + 1
+    print(file)
+    name = file.split("\\")[-2].split("_")[-3] + "_" + file.split("\\")[-2].split("_")[-2]
+    print(name)
+    newname = name+".png"
+    os.rename(file, newname)
+    print(i)
